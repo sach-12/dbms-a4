ddd15db56fd98ffea7df73d14d559202e193dd0f
sach-12 <sachinshankar2010@gmail.com>
2021-12-04 12:36:03 +0530

    completed the assignment

--- src/static/staff/js/staff.js
+++ src/static/staff/js/staff.js
@@ -35,6 +35,7 @@ function mtable(response) {
     var table = document.getElementById("mtable");
     for (const [key, value] of Object.entries(response)) {
         var row = table.insertRow();
+        row.id = "member_"+value["memberid"];
         var cell1 = row.insertCell();
         var cell2 = row.insertCell();
         var cell3 = row.insertCell();
@@ -44,15 +45,26 @@ function mtable(response) {
         var cell7 = row.insertCell();
         var cell8 = row.insertCell();
         var cell9 = row.insertCell();
+        var cell10 = row.insertCell();
         cell1.innerHTML = value["memberid"];
+        cell1.className = "memberid";
         cell2.innerHTML = value["mname"];
+        cell2.className = "mname";
         cell3.innerHTML = value["mphno"];
+        cell3.className = "mphno";
         cell4.innerHTML = value["doj"];
+        cell4.className = "doj";
         cell5.innerHTML = value["mplan"] + " Months";
+        cell5.className = "mplan";
         cell6.innerHTML = value["maddress"];
+        cell6.className = "maddress";
         cell7.innerHTML = value["tid"];
+        cell7.className = "tid";
         cell8.innerHTML = value["nutrid"];
+        cell8.className = "nutrid";
         cell9.innerHTML = value["gid"];
+        cell9.className = "gid";
+        cell10.innerHTML = "<button class='edit' onclick=getForm('member_"+value["memberid"]+"')>Edit</button>";
     }
 }
 
@@ -60,14 +72,21 @@ function ttable(response) {
     var table = document.getElementById("ttable");
     for (const [key, value] of Object.entries(response)) {
         var row = table.insertRow();
+        row.id = "trainer_"+value["trainerid"];
         var cell1 = row.insertCell();
         var cell2 = row.insertCell();
         var cell3 = row.insertCell();
         var cell4 = row.insertCell();
+        var cell5 = row.insertCell();
         cell1.innerHTML = value["trainerid"];
+        cell1.className = "trainerid";
         cell2.innerHTML = value["tname"];
+        cell2.className = "tname";
         cell3.innerHTML = value["tphno"];
+        cell3.className = "tphno";
         cell4.innerHTML = value["tsalary"];
+        cell4.className = "tsalary";
+        cell5.innerHTML = "<button class='edit' onclick=getForm('trainer_"+value["trainerid"]+"')>Edit</button>";
     }
 }
 
@@ -75,15 +94,23 @@ function ntable(response) {
     var table = document.getElementById("ntable");
     for (const [key, value] of Object.entries(response)) {
         var row = table.insertRow();
+        row.id = "nutritionist_"+value["nid"];
         var cell1 = row.insertCell();
         var cell2 = row.insertCell();
         var cell3 = row.insertCell();
         var cell4 = row.insertCell();
         var cell5 = row.insertCell();
+        var cell6 = row.insertCell();
         cell1.innerHTML = value["nid"];
+        cell1.className = "nid";
         cell2.innerHTML = value["nname"];
+        cell2.className = "nname";
         cell3.innerHTML = value["nincome"];
+        cell3.className = "nincome";
         cell4.innerHTML = value["nvd"];
+        cell4.className = "nvd";
         cell5.innerHTML = value["gid"];
+        cell5.className = "gid";
+        cell6.innerHTML = "<button class='edit' onclick=getForm('nutritionist_"+value["nid"]+"')>Edit</button>";
     }
 }
